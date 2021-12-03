83da8bbfc472f29768a0f55d5be598df36fc2af3
AndyGabler <andrewpgabler@gmail.com>
2021-12-03 15:53:46 -0500

    Model development finished as well as A4 reports.

--- A4/model_development.py
+++ A4/model_development.py
@@ -3,23 +3,25 @@ Model development per Task 8.
 
 @author: Andy Gabler & Kevin Spike
 """
-# Start Time 1:58 pm
+# Start Time 2:58 pm
 from pca import get_pca_components
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
+import pandas as pd
+import numpy as np
+import mglearn
+import matplotlib.pyplot as plt
 
 data, targets = get_pca_components()
 
 param_grid = {
-    'kernel' : ['rbf', 'linear', 'sigmoid', 'poly'], 
-    'degree' : [1, 2, 3, 4],
     'C': [0.001, 0.01, 0.1, 1, 10, 100], 
     'gamma': [0.001, 0.01, 0.1, 1, 10, 100],
-    'epsilon': [0.001, 0.01, 0.1, 1, 10, 100]
 }
 
-grid_search = GridSearchCV(SVR(), param_grid, cv=5)
+# Note, epsilon was originally in grid search but taken out to make a nicer heat map
+grid_search = GridSearchCV(SVR(kernel='linear', epsilon=100), param_grid, cv=5)
 X_train, X_test, y_train, y_test = train_test_split(
     data, targets, random_state=0
 )
@@ -31,3 +33,6 @@ print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
 print("Best estimator:\n{}".format(grid_search.best_estimator_))
 
 parameter_results = grid_search.cv_results_
+results = pd.DataFrame(parameter_results)
+scores = np.array(results.mean_test_score).reshape(6, 6)
+mglearn.tools.heatmap(scores, xlabel='gamma', xticklabels=param_grid['gamma'],ylabel='C', yticklabels=param_grid['C'], cmap="viridis")
